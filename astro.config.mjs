e3d68f3310f934128e9b00fd8c32fe692572b578
Irving Didier <bider88@gmail.com>
2025-05-15 21:40:26 -0600

    installing tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "http://localhost:4321",
+  vite: {
+    plugins: [tailwind()],
+  },
 });
